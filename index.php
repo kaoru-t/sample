0ddf83f292c3927c0383f12c3be74a92ef9a3794
kaoru-t <kaoru-t@yf7.so-net.ne.jp>
2016-01-25 14:19:03 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
  * 開発用ツールズサービス クライアント用WEBアプリケーション
  *
  */
-	include("inc/sql_devtools.php");
-	include("inc/common.php");
+    include("inc/sql_devtools.php");
+    include("inc/common.php");
 
-	redirect("/dev/login.php");
+    redirect("/dev/login.php");
 ?>
